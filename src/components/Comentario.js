f5150673f0a30130a71361aa17fca0774d5499a9
Alison Ghedin <alison.ghedin@ambevtech.com.br>
2021-02-23 23:54:56 -0300

    adicionando estilos

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Comentario.css'
+import imagemUsuario from './user.png'
 
 
 //JSX
 const Comentario = (props) => {
     return (
     <div className = "Comentario" >
-        <h2>Comentario</h2>
-        <p>{props.nome}</p>
-        <p>{props.email}</p>
-        <p>{props.children}</p>
-        <p>{props.data.toString()}</p>
+        <h2 className = "nome">{props.nome}</h2>
+        <img src={imagemUsuario} alt={props.nome} className="avatar"/>
+        <div>
+            <p className = "email">{props.email}</p>
+            <p className = "mensagem">{props.children}</p>
+            <p className = "data">{props.data.toString()}</p>
+        </div>
         <button onClick={props.onRemove}>&times;</button>
     </div>
     );
